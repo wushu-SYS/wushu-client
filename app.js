398ecbda2d0b832e306d86d04cde3c30f7de481b
alina <alinapolei96@gmail.com>
2019-08-07 23:19:28 +0300

    added client side and started login screen

---
+++ app.js
@@ -0,0 +1,29 @@
+let app = angular.module('myApp', ["ngRoute"]);
+app.controller("mainController", function ($scope, $location, $window, $rootScope) {
+    if($window.sessionStorage.getItem('name') != null && $window.sessionStorage.getItem('name')!=='')
+        $rootScope.name = $window.sessionStorage.getItem('name');
+    else
+        $rootScope.name = "guest";
+
+    $scope.logout = function () {
+        //need delete $rootScope
+        $window.sessionStorage.removeItem('name');
+        $window.sessionStorage.removeItem('token');
+        $rootScope.name = "guest";
+        $location.path('/home');
+    }
+});
+
+// config routes
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'loginController as loginCtrl'
+        })
+        .otherwise({redirectTo: '/home'});
+});
---
+++ controllers/login.js
@@ -0,0 +1,25 @@
+app.controller("loginController", function ($scope, $http, $window, $location, $rootScope) {
+    serverUrl = "http://localhost:3000";
+    $scope.submit = function(isValid){
+        if(isValid){
+            var user ={
+                username : $scope.username,
+                password : $scope.password};
+            /*$http.post(serverUrl+ '/login', user)
+                .then(function (response) {
+                        if(response.data == "Wrong username or password")
+                            $scope.isError = true;
+                        else {
+                            $rootScope.name = $scope.username;
+                            $window.sessionStorage.setItem('name', $scope.username);
+                            $window.sessionStorage.setItem('token', response.data);
+                            $location.path('/home');
+                        }
+                    },
+                    function (error) {
+                        $scope.isError = true;
+                        console.log(error.data);
+                    });*/
+        }
+    };
+});
