ae86fb4a84e914fb8ae3754b74e71f8f57faf082
alina <alinapolei96@gmail.com>
2019-08-09 21:39:50 +0300

    finish menu bar

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ let app = angular.module('myApp', ["ngRoute"]);
 app.controller("mainController", function ($scope, $location, $window, $rootScope) {
     if($window.sessionStorage.getItem('name') != null && $window.sessionStorage.getItem('name')!=='')
         $rootScope.name = $window.sessionStorage.getItem('name');
-    else
-        $rootScope.name = "guest";
+
+    $scope.getClass = function (path) {
+        return ("/" + $location.path().split("/")[1] === path) ? 'active' : '';
+    };
 
     $scope.logout = function () {
-        //need delete $rootScope
+        //need to delete $rootScope
         $window.sessionStorage.removeItem('name');
         $window.sessionStorage.removeItem('token');
         $rootScope.name = "guest";
@@ -25,9 +27,73 @@ app.config(function($routeProvider) {
             templateUrl: 'views/login.html',
             controller: 'loginController as loginCtrl'
         })
-        .when('/register',{
+        .when('/users/register',{
             templateUrl :'views/register.html',
             controller: 'registerController as chRegCtrl'
+        })
+        .when('/calendar', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/profile', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/sportClubs/addSportClub', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/sportClubs/sportClubs', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/users/couches', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/users/admins', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/users/sportsman', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/users/referees', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/competitions/addCompetition', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/competitions/registerToCompetition', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/competitions/addResults', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/competitions/results', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/events/addEvent', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/events/addMessage', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/events/events', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
+        })
+        .when('/events/messages', {
+            templateUrl: 'views/home.html',
+            controller: 'homeController as hCtrl'
         })
         .otherwise({redirectTo: '/login'});
 });
