4e8cb7ddf0d33c93ff224352d28672d013718066
alina <alinapolei96@gmail.com>
2019-08-31 17:38:08 +0300

    search bar client+server

--- controllers/sportsmen.js
+++ controllers/sportsmen.js
@@ -1,21 +1,9 @@
 app.controller("sportsmenController", function ($scope, $http, $window, clubService, pagingService) {
     serverUrl = "http://localhost:3000";
     var allUsers;
-    var req = {
-        method: 'POST',
-        url: serverUrl + '/private/getSportsmen',
-        headers: {
-            'x-auth-token': $window.sessionStorage.getItem('token')
-        },
-    };
-    $http(req).then(function (result) {
-        allUsers = result.data;
-        setPage(1);
-    }, function (error) {
-        console.log(error)
-    });
-
-
+    $scope.pager = {};
+    $scope.isToDesc = true;
+    setPage(1);
     getDataForDisplay();
 
     // async function getDataForDisplay() {
@@ -38,13 +26,55 @@ app.controller("sportsmenController", function ($scope, $http, $window, clubServ
     };
 
     function setPage(page) {
-        if (page < 1 || page > $scope) {
+        if (page < 1 || (page > $scope.pager.totalPages && $scope.totalPages > 0)) {
             return;
+            console.log("exit");
         }
 
-        $scope.pager = pagingService.GetPager(allUsers.length, page, pageSize);
+        //$scope.pager = pagingService.GetPager(allUsers.length, page, pageSize);
 
-        $scope.users = allUsers.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
+        var req = {
+            method: 'POST',
+            url: serverUrl + '/private/getSportsmen' + buildConditions(),
+            headers: {
+                'x-auth-token': $window.sessionStorage.getItem('token')
+            },
+        };
+        $http(req).then(function (result) {
+            console.log(serverUrl + '/private/getSportsmen' + buildConditions());
+            allUsers = result.data;
+
+            $scope.pager = pagingService.GetPager(allUsers.length, page, pageSize);
+            $scope.users = allUsers.slice($scope.pager.startIndex, $scope.pager.endIndex + 1);
+        }, function (error) {
+            console.log(error)
+        });
+    }
+    function buildConditions(){
+        var conditions = [];
+
+        if($scope.searchText !== null && $scope.searchText !== undefined) {
+            conditions.push('value=' + $scope.searchText);
+        }
+        if($scope.selectedBranch !== null && $scope.selectedBranch !== undefined){
+            conditions.push('branch=' + $scope.selectedBranch.name);
+        }
+        if($scope.selectedClub != null && $scope.selectedClub !== undefined){
+            conditions.push('club=' + $scope.selectedClub.id);
+        }
+        if($scope.selectedSex !== null && $scope.selectedSex !== undefined){
+            conditions.push('sex=' + $scope.selectedSex.name);
+        }
+        if($scope.isToDesc === false){
+            conditions.push('sort=desc')
+        }
+
+        return conditions.length ? '?' + conditions.join('&') : '';
+    }
+
+    $scope.sortStyleChanged = function (){
+        $scope.isToDesc = !$scope.isToDesc;
+        setPage(1);
     }
 
 });
