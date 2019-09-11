81d109f048a97a81762164e104ffd443522a20a9
droru <droruri3@gmail.com>
2019-09-11 18:00:49 +0300

    start competition register

--- controllers/competitionTableController.js
+++ controllers/competitionTableController.js
@@ -3,7 +3,6 @@ app.controller("competitionTableController", function($scope, $window, $http,com
     serverUrl = "http://localhost:3000";
     $scope.headerTable = "תחרויות";
     $scope.pager = {};
-
      competitionService.getCompetitons()
         .then(function (result) {
             $scope.competitions = result.data;
@@ -16,6 +15,19 @@ app.controller("competitionTableController", function($scope, $window, $http,com
         $uibModal.open({
             templateUrl: "views/competitonDetails.html",
             controller: "competitionDetailsModal as cDetailsCtrl",
+            backdrop  : 'static',
+            keyboard  : false,
+            resolve: {
+                getId: function () {
+                    return idCompetiton;
+                }
+            }
+        });
+    };
+    $scope.regSportsman = function (idCompetiton) {
+        $uibModal.open({
+            templateUrl: "views/regSportsmanCompetition.html",
+            controller: "competitionRegisterModal as cRegCtrl",
             backdrop  : 'static',
             keyboard  : false,
             resolve: {
