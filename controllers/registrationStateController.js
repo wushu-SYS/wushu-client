a2831acfa11df863c92003cebf65f916cbd38749
alina <alinapolei96@gmail.com>
2019-09-27 22:59:22 +0300

    client side - state of registration

--- controllers/registrationStateController.js
+++ controllers/registrationStateController.js
@@ -1,13 +1,31 @@
 app.controller("registrationStateController", function($scope, $window, $http, sportsmanService, $routeParams) {
-    getUser();
+    getDisplayData();
 
-    function getUser(){
+    function getDisplayData(){
         sportsmanService.getSportsmen(sportsmanService.buildConditionds("", null, null, null, null, $routeParams.idComp, '%3D%3D'))
             .then(function (result) {
                 $scope.users = result.data.sportsmen;
-                console.log($scope.users);
+            }, function (error) {
+                console.log(error)
+            });
+
+        sportsmanService.getCategories()
+            .then(function (result) {
+                $scope.categories = result.data;
             }, function (error) {
                 console.log(error)
             });
     }
+});
+
+app.filter('categoryFilter', function() {
+    return function( items, user) {
+        var filtered = [];
+        angular.forEach(items, function(item) {
+            if( user.age >= item.minAge && (item.maxAge == null ||  user.age <= item.maxAge) && user.sex == item.sex) {
+                filtered.push(item);
+            }
+        });
+        return filtered;
+    };
 });
