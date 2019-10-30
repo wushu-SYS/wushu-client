11f408b1bbec3b4a1f2ccfc5cf31e5c9c5ee1855
alina <alinapolei96@gmail.com>
2019-10-30 22:07:10 +0200

    excel error text

--- controllers/register.js
+++ controllers/register.js
@@ -6,7 +6,6 @@ app.controller("registerController", function ($scope, $http, $window, $location
     $scope.isRegisterCoach = false;
     $scope.coaches = new Array()
     $scope.clubs = new Array();
-    var changeExcel = document.getElementById("changeExcel");
     let dropZoneRegisterUsers = document.getElementById("dropZoneRegisterUsers")
 
 
@@ -44,10 +43,10 @@ app.controller("registerController", function ($scope, $http, $window, $location
     fillDataTmpFunction();
 
 
-    changeExcel.onclick = function (e) {
-        e.preventDefault()
+    $scope.uploadNewFile = function(){
+        $scope.excelErrors = [];
+        $scope.isDropped = false;
         dropZoneRegisterUsers.className = "dropzone"
-        changeExcel.style.display = "none"
         document.getElementById("dropText").innerHTML = "גרור קובץ או לחץ על העלאת קובץ";
         document.getElementById("fileSportsman").value = "";
     }
@@ -69,8 +68,8 @@ app.controller("registerController", function ($scope, $http, $window, $location
     function changeDropZone(name) {
         var droptext = document.getElementById("dropText");
         droptext.innerHTML = name.toString();
+        $scope.isDropped = true;
         dropZoneRegisterUsers.className = "dropzoneExcel"
-        changeExcel.style.display = "block"
     }
 
     dropZoneRegisterUsers.ondragover = function () {
@@ -118,11 +117,6 @@ app.controller("registerController", function ($scope, $http, $window, $location
         }
     };
 
-    function replacer(key, value) {
-        return value.split(',').join('\n');
-    }
-
-
     function registerUsers(data, isRegisterCoach) {
         if (!isRegisterCoach)
             registerService.registerUsers(data)
@@ -131,7 +125,8 @@ app.controller("registerController", function ($scope, $http, $window, $location
                     $location.path("/home");
                 })
                 .catch((err) => {
-                    console.log(err)
+                    console.log(err);
+                    $scope.excelErrors = err.data;
                 })
     }
 
@@ -146,22 +141,6 @@ app.controller("registerController", function ($scope, $http, $window, $location
         $scope.sportStyle = 'טאולו'
         $scope.birthdate = new Date(1990, 3, 3);
     }
-
-    function displayMsgForRegister(collectionErr) {
-        console.log(collectionErr)
-        $scope.resErr = true;
-        let err = '';
-        collectionErr.forEach((e) => {
-
-            let colErr = ''
-            e.errors.forEach((ee) => {
-                colErr = colErr + "\n" + ee + "\n"
-            })
-            err = err + "\n" + e.id + colErr
-        })
-        $scope.errorText = err;
-    }
-
 
     function clearFields() {
         /*$scope.id = "";
