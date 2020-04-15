8c40541b101d36e86ce7b228793cef07637774ba
alina <alinapolei96@gmail.com>
2020-04-15 17:08:38 +0300

    show competition results

--- controllers/competitionResults/competitionResultsTaulloController.js
+++ controllers/competitionResults/competitionResultsTaulloController.js
@@ -1,12 +1,12 @@
 app.controller("competitionResultsTaulloController", function ($scope, $http, $routeParams, $window, $location, constants, SocketService, competitionService, categoryService) {
     getDisplayData()
     function getDisplayData() {
-        // competitionService.getResultCompetition($routeParams.idComp)
-        //     .then(function (result) {
-        //         $scope.compResults = result.data;
-        //     }, function (error) {
-        //         console.log(error);
-        //     })
+        competitionService.getResultCompetition($routeParams.idComp)
+            .then(function (result) {
+                $scope.compResults = result.data;
+            }, function (error) {
+                console.log(error);
+            })
     }
 
 
