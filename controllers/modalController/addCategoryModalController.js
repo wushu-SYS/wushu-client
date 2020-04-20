6b89800c072c292729ebac5d31396f2edc06ba1a
alina <alinapolei96@gmail.com>
2020-04-20 16:05:18 +0300

    set category by min year and max year

--- controllers/modalController/addCategoryModalController.js
+++ controllers/modalController/addCategoryModalController.js
@@ -1,6 +1,26 @@
 app.controller("addCategoryModalController", function($scope, $window, $uibModalInstance, $http,competitionService, toastNotificationService, constants) {
     $scope.sexEnum = constants.sexEnum;
     $scope.regex = constants.regex;
+    $scope.currYear = new Date().getFullYear();
+
+    $scope.onSetAge = function(){
+        if($scope.minAge && !$scope.maxAge)
+            $scope.maxAge = $scope.minAge
+        else if(!$scope.minAge && $scope.maxAge)
+            $scope.minAge = $scope.maxAge
+
+        $scope.minYear = $scope.currYear - $scope.maxAge;
+        $scope.maxYear = $scope.currYear - $scope.minAge;
+    }
+    $scope.onSetYear = function(){
+        if($scope.minYear && !$scope.maxYear)
+            $scope.maxYear = $scope.minYear
+        else if(!$scope.minYear && $scope.maxYear)
+            $scope.maxYear = $scope.minYear
+
+        $scope.minAge = $scope.currYear - $scope.maxYear;
+        $scope.maxAge = $scope.currYear - $scope.minYear;
+    }
 
     $scope.submit =function (isValid) {
         if(isValid){
