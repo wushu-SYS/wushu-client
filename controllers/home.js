0d66e9b681cdf007188f5ef0150b8c7ed594e547
alina <alinapolei96@gmail.com>
2020-05-07 11:11:45 +0300

    fix ticker package

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,4 @@
-app.controller("homeController", function ($scope, $uibModal, $window, constants) {
+app.controller("homeController", function ($scope, $uibModal, $window, constants, $interval, $timeout, clubService) {
     $scope.changePassword = function () {
             $uibModal.open({
                 templateUrl: "views/modalView/changePasswordModal.html",
@@ -13,24 +13,41 @@ app.controller("homeController", function ($scope, $uibModal, $window, constants
 
     getDisplayData();
     function getDisplayData(){
-        $scope.myTickerItems = [
-            {
-                text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-                creationDate: '02/02/2020'
-            },
-            {
-                text: 'אלינה דרור',
-                creationDate: '02/02/2020'
-            },
-            {
-                text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
-                creationDate: '02/02/2020'
-            },
-            {
-                text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
-                creationDate: '02/02/2020'
-            }
-        ]
+        clubService.getClubs()
+            .then(function (result) {
+                $scope.myTickerItems = [
+                    {
+                        text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+                        creationDate: '02/02/2020'
+                    },
+                    {
+                        text: 'אלינה דרור',
+                        creationDate: '02/02/2020'
+                    },
+                    {
+                        text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+                        creationDate: '02/02/2020'
+                    },
+                    {
+                        text: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+                        creationDate: '02/02/2020'
+                    }
+                ]
+            });
     }
+
+
+    $scope.moving = false;
+    $scope.moveLeft = function() {
+        $scope.moving = true;
+        $timeout($scope.switchFirst, 1000);
+    };
+    $scope.switchFirst = function() {
+        $scope.myTickerItems.push($scope.myTickerItems.shift());
+        $scope.moving = false;
+        $scope.$apply();
+    };
+    $interval($scope.moveLeft, 2000);
+
 
 });
