bff54acc0d84da73af7a8122519a4cf3c1819300
dror <droruri3@gmail.com>
2020-09-19 17:50:12 +0300

    add edit coaches details by excel

--- controllers/addFormController/editUsersByExcelController.js
+++ controllers/addFormController/editUsersByExcelController.js
@@ -1,11 +1,19 @@
 app.controller("editUsersByExcelController", function ($scope, $rootScope,$routeParams,$filter,$location, competitionService, constants,$window,confirmDialogService, toastNotificationService) {
     $scope.userType = $routeParams.userType
     let downloadExcelLinkEditSportsman = document.getElementById("downExcelEditSportsman");
+    let downloadExcelLinkEditCoaches = document.getElementById("downExcelEditCoaches");
 
     $scope.downloadExcelEditSportsMan = function () {
         let token = $window.sessionStorage.getItem('token')
         let url = constants.serverUrl + '/downloadExcelEditSportsmanDetails/' + token;
         downloadExcelLinkEditSportsman.setAttribute('href', url);
         downloadExcelLinkEditSportsman.click();
+    };
+
+    $scope.downloadExcelEditCoaches = function () {
+        let token = $window.sessionStorage.getItem('token')
+        let url = constants.serverUrl + '/downloadExcelEditCoachesDetails/' + token;
+        downloadExcelLinkEditCoaches.setAttribute('href', url);
+        downloadExcelLinkEditCoaches.click();
     };
 });
