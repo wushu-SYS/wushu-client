24e06b42163ca72dff388d3a55b7177fedaed258
alina <alinapolei96@gmail.com>
2020-11-07 17:19:11 +0200

    fixes #2

--- controllers/profileController/sportsmanProfile.js
+++ controllers/profileController/sportsmanProfile.js
@@ -272,7 +272,7 @@ app.controller("sportsmanProfileController", function ($scope, $http, $filter, $
         if ($rootScope.access != $rootScope.userTypes.SPORTSMAN)
             sportsmanService.getSportsmanRank({id: parseInt($routeParams.id)})
                 .then((function (result) {
-                    $scope.sportsmanRank = result.data[0].avg_rank ? result.data[0].avg_rank : "-"
+                    $scope.sportsmanRank = result.data[0].avg_rank ? result.data[0].avg_rank : "0"
                 }), function (err) {
                     console.log(err)
                 })
